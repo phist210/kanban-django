904b9bf311a58a090c542b063535e3e44d577dc1
Josh Friese <Friese@Friese.local>
2017-04-03 16:33:57 -0400

    configured database url

--- kanban_project/settings.py
+++ kanban_project/settings.py
@@ -100,6 +100,7 @@ DATABASES = {
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
