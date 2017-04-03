926000aff5cb0605b71131ddedc58ae269c8e2fc
Josh Friese <Friese@Friese.local>
2017-04-03 16:31:05 -0400

    amended settings for heroku deployment

--- kanban_project/settings.py
+++ kanban_project/settings.py
@@ -150,3 +150,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
